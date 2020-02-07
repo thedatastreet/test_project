c49bc4ea9edebbecc05cceff9944b19a3a0ef7c2
thedatastreet <thedatastreet@gmail.com>
2020-02-07 09:52:50 +0530

    add program in python

---
+++ add.py
@@ -0,0 +1,10 @@
+# This is a python program to add two numbers
+
+def adder(number_one, number_two):
+    '''
+    adder functions adds two numbers and return their sum
+    '''
+    return number_one+number_two
+
+
+print(adder(10,20))
